1385e806fc243f1916667875ac21a82c8b3c8824
Ondrej Stieber <ondrej.stieber@tsium.com>
2019-05-08 22:25:42 +0200

    Time related movement

--- shaders/task3_uniform_time/start.vert
+++ shaders/task3_uniform_time/start.vert
@@ -1,8 +1,10 @@
 #version 150
 in vec2 inPosition; // input from the vertex buffer
+out vec3 vertColor; // výstupní barva
 
 uniform mat4 proj;
 uniform mat4 view;
+uniform float time;
 
 const float PI = 3.14;
 
@@ -12,18 +14,21 @@ vec3 getSphere(vec2 xy) {
 	float ze = xy.y * PI / 2; // máme od -1 do 1 a chceme od -PI/2 do PI/2
 	float r = 1;
 
-	float x = cos(az) * cos(ze) *r;
-	float y = 2*sin(az) * cos(ze) *r;
-	float z = 0.5 * sin(ze) *r;
+	float x = cos(az) * cos(ze) * r;
+	float y = 2*sin(az) * cos(ze) * r;
+	float z = 0.5 * sin(ze) * r;
 
 	return vec3(x, y, z);
 }
 
 void main() {
 	vec2 pos = inPosition * 2 - 1;
-	vec3 finalPos;
+	vec3 finalPos = getSphere(pos);
 
-	finalPos = getSphere(pos);
+    vertColor = finalPos;
+
+	finalPos.x += 0.1;
+	finalPos.y += cos(finalPos.x + time);
 
 	gl_Position = proj * view * vec4(finalPos, 1.0);
 }
--- src/task3_uniform_time/JOGLApp.java
+++ src/task3_uniform_time/JOGLApp.java
@@ -5,8 +5,6 @@ import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLProfile;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.util.FPSAnimator;
-import common.BasicRenderer;
-import common.FrameFactory;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,13 +12,46 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class JOGLApp {
-
+	private static final int FPS = 60; // animator's target frames per second
 
 	private void start() {
 		try {
-			BasicRenderer renderer = new BasicRenderer("/task3_uniform_time/start.vert",
-					"/task3_uniform_time/start.frag");
-			FrameFactory.GetApplicationFrame(renderer);
+			Frame testFrame = new Frame("TestFrame");
+			testFrame.setSize(512, 384);
+
+			// setup OpenGL version
+			GLProfile profile = GLProfile.getMaximum(true);
+			GLCapabilities capabilities = new GLCapabilities(profile);
+
+			// The canvas is the widget that's drawn in the JFrame
+			GLCanvas canvas = new GLCanvas(capabilities);
+			Renderer ren = new Renderer();
+			canvas.addGLEventListener(ren);
+			canvas.addMouseListener(ren);
+			canvas.addMouseMotionListener(ren);
+			canvas.addKeyListener(ren);
+			canvas.setSize( 512, 384 );
+
+			testFrame.add(canvas);
+
+			//final Animator animator = new Animator(canvas);
+			final FPSAnimator animator = new FPSAnimator(canvas, FPS, true);
+
+			testFrame.addWindowListener(new WindowAdapter() {
+				@Override
+				public void windowClosing(WindowEvent e) {
+					new Thread(() -> {
+						if (animator.isStarted()) animator.stop();
+						System.exit(0);
+					}).start();
+				}
+			});
+			testFrame.setTitle(ren.getClass().getName());
+			testFrame.pack();
+			testFrame.setVisible(true);
+			animator.start(); // start the animation loop
+
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
